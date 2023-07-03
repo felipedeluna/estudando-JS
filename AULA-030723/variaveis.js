e83751aff73f894e172e20fc8c03e71cb2168a6b
felipedeluna <fcoladodeluna@gmail.com>
2023-07-03 12:02:16 -0300

    Variaveis de JS

---
+++ AULA-030723/variaveis.js
@@ -0,0 +1,40 @@
+// Java -> Linguagem de Programação (Compilada)
+// JavaScript -> Linguagem de Programação (Interpretada)
+
+// chrome -> V8 -> Interpretador do JS
+// Node.js -> V8 + NPM (Node Package Manager)
+
+// console.log("Texto") //Imprime no terminal/console uma informação
+// console.log(5)
+
+
+// Variáveis -> Espaço (na memória) para armazenar um valor
+
+let nome; // Declaração da variável nome
+nome = "Henrique Meirelles"; // Atribuição do valor à variável
+console.log(nome);
+
+let idade = 23;
+console.log(idade);
+
+let altura = 1.74;
+console.log(altura);
+
+let cpf = '123.456.789-00';
+console.log(cpf);
+
+let ecasado = true; //true or false
+console.log(ecasado);
+
+// Substitui o valor 23 por 24
+idade = 24;
+console.log(idade);
+
+const PI = 3.14; // Declara a atribuição de uma constante
+console.log(PI);
+
+// PI = 14; isso vai gerar um erro
+// console.log(PI)
+
+// Tipagem estática -> Definição de um tipo a cada variável (Java, C, C++, TypeScript)
+// Tipagem dinâmica -> O tipo da variável é definido de acordo com seu valor (JavaScript, Python)
